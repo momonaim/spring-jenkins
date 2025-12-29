4271aec3be764b0203e3b33594e7c6d9cc2dff23
momonaim <157602657+momonaim@users.noreply.github.com>
2025-12-29 20:14:23 +0100

    Initial Spring Boot hello world

---
+++ src/main/java/com/fsts/spring_jenkins/SpringJenkinsApplication.java
@@ -0,0 +1,12 @@
+package com.fsts.spring_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+}
